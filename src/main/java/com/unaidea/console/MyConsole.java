684fba10acc592d4152f9bc68d26d65d67c146e2
joscarranzs <jcarranzsosa@gmail.com>
2024-12-05 04:28:46 -0500

    Method chaining is applied.

--- src/main/java/com/unaidea/console/MyConsole.java
+++ src/main/java/com/unaidea/console/MyConsole.java
@@ -33,6 +33,80 @@ public class MyConsole {
     }
 
     /**
+     * Method to clean the console, it works on linux and windows.
+     * @return The MyConsole object.
+     */
+
+    public MyConsole cleanLine() {
+        try {
+            if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    /**
+     * Prints a message to the console.
+     *
+     * @param message The message to print.
+     * @return The console instance.
+     */
+
+    public MyConsole print(String message) {
+        System.out.println(message);
+        return this;
+    }
+
+    /**
+     * Prints an array of messages to the console.
+     *
+     * @param messages The messages to print.
+     * @return The console instance.
+     */
+
+    public MyConsole print(String[] messages) {
+        for (String message : messages) {
+            System.out.println(message);
+        }
+        return this;
+    }
+
+    /**
+     * Prints a single object to the console.
+     *
+     * @param message The object to print.
+     * @param <T>     The type of the object.
+     * @return The console instance.
+     */
+
+    public <T> MyConsole print(T message) {
+        System.out.println(message);
+        return this;
+    }
+
+    /**
+     * Prints a message and a list of objects to the console.
+     *
+     * @param message The message to print.
+     * @param list    The list of objects to print.
+     * @param <T>     The type of the objects.
+     * @return The console instance.
+     */
+
+    public <T> MyConsole print(String message, List<T> list) {
+        System.out.println(message);
+        for (T element : list) {
+            System.out.print(element + " ");
+        }
+        return this;
+    }
+
+    /**
      * Method to print a message on the console.
      *
      * @param message The function receives a message and prints it in the console.

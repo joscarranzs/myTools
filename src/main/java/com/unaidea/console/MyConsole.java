70d7d509712a7e2f7ab4c4314e44353e06bd1dd7
joscarranzs <jcarranzsosa@gmail.com>
2024-12-04 21:03:41 -0500

    A new feature has been added.

--- src/main/java/com/unaidea/console/MyConsole.java
+++ src/main/java/com/unaidea/console/MyConsole.java
@@ -1,6 +1,10 @@
 package com.unaidea.console;
 
 public class MyConsole {
+    /**
+     * Method to clean the console, it works on linux and windows.
+     */
+
     public void clean() {
         try {
             if (System.getProperty("os.name").toLowerCase().contains("windows")) {
@@ -13,7 +17,24 @@ public class MyConsole {
         }
     }
 
+    /**
+     * Method to print a message on the console.
+     *
+     * @param message The function receives a message and prints it in the console.
+     */
     public void printMessage(String message) {
         System.out.println(message);
+    }
+
+    /**
+     * Method to print multiple messages in the console.
+     *
+     * @param messages The function receives an array of messages and prints them in the console.
+     */
+
+    public void printMessage(String[] messages) {
+        for (String message : messages) {
+            System.out.println(message);
+        }
     }
 }

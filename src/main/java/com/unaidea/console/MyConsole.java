d288799b66a2dd8eddd7ec6831bfbf6faf3332fd
joscarranzs <jcarranzsosa@gmail.com>
2024-12-04 21:20:55 -0500

    The first methods for data readings were added.

--- src/main/java/com/unaidea/console/MyConsole.java
+++ src/main/java/com/unaidea/console/MyConsole.java
@@ -1,3 +1,11 @@
+/**
+ * The myConsole class attempts to facilitate interaction with the console.
+ *
+ * @author Jose Carranza
+ * @since 1.0
+ */
+
+
 package com.unaidea.console;
 
 public class MyConsole {
@@ -35,6 +43,37 @@ public class MyConsole {
     public void printMessage(String[] messages) {
         for (String message : messages) {
             System.out.println(message);
+        }
+    }
+
+    /**
+     * Method to request input of an integer value.
+     *
+     * @return Returns the integer value entered by the user.
+     */
+
+    public int getIntegerInput() {
+        try {
+            return Integer.parseInt(System.console().readLine());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    /**
+     * Method to request input of an integer value.
+     *
+     * @param message The function receives a message and prints it in the console.
+     * @return Returns the integer value entered by the user.
+     */
+
+    public int getIntegerInput(String message) {
+        try {
+            return Integer.parseInt(System.console().readLine(message));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
         }
     }
 }

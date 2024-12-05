20c7dfe3cdfcd24b287d0f6ee818f080b5e8bec2
joscarranzs <jcarranzsosa@gmail.com>
2024-12-04 19:19:49 -0500

    Start

---
+++ src/main/java/com/unaidea/App.java
@@ -0,0 +1,7 @@
+package com.unaidea;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Mi primer package subido a github");
+    }
+}
---
+++ src/main/java/com/unaidea/console/MyConsole.java
@@ -0,0 +1,15 @@
+package com.unaidea.console;
+
+public class MyConsole {
+    public void clean() {
+        try {
+            if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
